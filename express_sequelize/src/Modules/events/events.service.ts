e3c6cf6dc3b91260a385b32979c748783d12bdbf
Mayank <mynkkum15@gmail.com>
2022-10-21 17:03:10 +0530

    Added generic method for formatting the events.

--- express_sequelize/src/Modules/events/events.service.ts
+++ express_sequelize/src/Modules/events/events.service.ts
@@ -86,37 +86,40 @@ export class EventsService {
     ]
     ```
      */
-    
+  
+  formatEvents(events: any) {
+    let result: any = {}  
+    events.forEach((event: any) => {
+      const eventId = event.eventId;
+      const workShopData = {
+        id: event.workshopId,
+        name: event.workshopName,
+        start: event.start,
+        end: event.end,
+        createdAt: event.workshopCreatedAt,
+        eventId
+      }
+      if (result[eventId]) {
+        result[eventId].workshops.push(workShopData)
+      } else {
+        result[eventId] = {
+          id: eventId,
+          name: event.name,
+          createdAt: event.createdAt,
+          workshops: [workShopData]
+        } 
+      }
+    })
+
+    return Object.values(result);
+  } 
+
   async getEventsWithWorkshops() {
     const sqlQuery = "Select E1.id, E1.name, E1.createdAt, workshop_data.* from Event E1 Join (Select id as workshopId, start,end, eventId, name as workshopName, createdAt as workshopCreatedAt from Workshop) workshop_data on workshop_data.eventId = E1.id";
     const app = Server.getApp()
     try {
       const events = await app.getDataSource().query(sqlQuery, { type: QueryTypes.SELECT });
-      let result: any = {}  
-      events.forEach((event: any) => {
-        const eventId = event.eventId;
-        const workShopData = {
-          id: event.workshopId,
-          name: event.workshopName,
-          start: event.start,
-          end: event.end,
-          createdAt: event.workshopCreatedAt,
-          eventId
-        }
-        if (result[eventId]) {
-          result[eventId].workshops.push(workShopData)
-        } else {
-          result[eventId] = {
-            id: eventId,
-            name: event.name,
-            createdAt: event.createdAt,
-            workshops: [workShopData]
-          } 
-        }
-      })
-
-      const response = Object.values(result);
-      return response
+      return this.formatEvents(events)
     } catch(err) {
       console.log('error ======>', err)
     }
@@ -193,32 +196,7 @@ export class EventsService {
     const app = Server.getApp()
     try {
       const events = await app.getDataSource().query(sqlQuery, { type: QueryTypes.SELECT });
-      let result: any = {}  
-      events.forEach((event: any) => {
-        const eventId = event.eventId;
-        const workShopData = {
-          id: event.workshopId,
-          name: event.workshopName,
-          start: event.start,
-          end: event.end,
-          createdAt: event.workshopCreatedAt,
-          eventId
-        }
-        if (result[eventId]) {
-          result[eventId].workshops.push(workShopData)
-        } else {
-          result[eventId] = {
-            id: eventId,
-            name: event.name,
-            createdAt: event.createdAt,
-            workshops: [workShopData]
-          } 
-        }
-      })
-
-      const response = Object.values(result);
-      console.log(JSON.stringify(response, null, 10))
-      return response
+      return this.formatEvents(events)
     } catch(e) {
       console.log(e)
     }  
